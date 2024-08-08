81a0ee26f27e87d7ce91aac3ccf24170c98096f5
vishal tiwari <vishaltiwari13200@gmail.com>
2024-08-08 20:20:53 +0530

    New Update 116

---
+++ Backend/asset.php
@@ -0,0 +1,25 @@
+<?php
+    // Database Connection
+
+    $server="localhost";
+
+    // Cpane Url="https://Adrengroup.in/c-panel or Adrengroup.in:2088"
+
+    // ---PORT : 2088 --------
+    // ---HOST ON NAMECHEAP-----
+    // ---DOMAIN REGISTERATION ON 07 JULY ON HOSTINGER---
+    // ---NO CLIENT MAIL INSERTED --------
+
+    $Adern_user_identification="adreazwi";
+    $Adern_user_password="1qleetI9VriL";
+    $Adren_user_Database_Cn_name="db_adren_user_311323";
+
+    //  NO Database Password Instered
+    
+
+
+
+
+    // Session Authentication JWT Token
+
+?>
